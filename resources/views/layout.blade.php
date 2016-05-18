b43f255e9d89fd1dd8e4514de12d78ce2270801f
Aidil <aidil@pocketpixel.com>
2016-05-18 16:23:36 +0800

    footer center

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -18,7 +18,7 @@
         @yield('content')
 
         <footer>
-            © Workwork, Pocket Pixel, Sdn Bhd. 2016.
+            <small class="copyright text-xs-center m-b-3">© Workwork, Pocket Pixel, Sdn Bhd. 2016.</small>
         </footer>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
