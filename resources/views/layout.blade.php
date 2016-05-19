6536d128dec7416800b6dd38450f88b91a431cde
Aidil <aidil@pocketpixel.com>
2016-05-19 23:43:59 +0800

    og image

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,9 +8,10 @@
         <meta property="fb:app_id" content="1578992975731734" />
         <meta property="fb:admins" content="622633523" />
         <meta property="og:title" content="WorkWork - portal pencarian kerja part-time" />
+        <meta property="og:description" content="Jika anda mencari kerja part-time atau sedang mencari pekerja part-time, di sinilah tempatnya! #kerjakerjakerja" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="http://www.workwork.my/" />
-        <meta property="og:image" content="http://www.workwork.my/images/logo.png" />
+        <meta property="og:image" content="http://www.workwork.my/images/fb-image.jpg" />
         <meta property="og:locale" content="ms_MY" />
         <meta property="og:locale:alternate" content="en_GB" />
         <meta property="og:site_name" content="WorkWork" />
