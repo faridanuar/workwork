93d690a18e849035ac52c370a65f067cc8fcc84f
Aidil <aidil@pocketpixel.com>
2016-05-20 15:45:29 +0800

    error 404 v1

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,49 +1,23 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Sesat.</title>
+@extends('layout')
 
-        <link href='https://fonts.googleapis.com/css?family=Josefin+Slab:700' rel='stylesheet' type='text/css'>
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                color: #B0BEC5;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 72px;
-                margin-bottom: 40px;
-            }
-            h1 {font-size: 3em;}
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <h1>404</h1>
-                <div class="title">Sesat? <a href="/"> Kembalilah ke pangkal jalan. </a></div>
+@section('content')
+    <header>
+        <div class="brand-ww">
+            <div class="title text-xs-center"><img src="images/logo.png" alt="" width="187"/></div>
+        </div>
+    </header>
+    <div class="container-fluid">
+        <div class="content">
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4 text-xs-center m-t-3 m-b-3">
+                    <h1>404</h1>
+                    <div class="title">Sesat? <a href="/"> Kembalilah ke pangkal jalan. </a></div>
+                    <p>
+                        <img src="images/logo-symbol.png" alt="workwork logo" width="88" />
+                    </p>
+                </div>
             </div>
         </div>
-    </body>
-</html>
+    </div>
+
+@stop
