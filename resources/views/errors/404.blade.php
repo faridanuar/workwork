449fd6e62609766230c5aab02d13f198184f2b2c
Aidil <aidil@pocketpixel.com>
2016-05-20 15:48:16 +0800

    error 404 v1.1

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -12,9 +12,7 @@
                 <div class="col-md-4 col-md-offset-4 text-xs-center m-t-3 m-b-3">
                     <h1>404</h1>
                     <div class="title">Sesat? <a href="/"> Kembalilah ke pangkal jalan. </a></div>
-                    <p>
-                        <img src="images/logo-symbol.png" alt="workwork logo" width="88" />
-                    </p>
+
                 </div>
             </div>
         </div>
