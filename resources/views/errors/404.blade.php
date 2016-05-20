ceeb87a2fde0f0f26e345709b395bdd86054d500
Aidil <aidil@pocketpixel.com>
2016-05-20 15:40:26 +0800

    error msg

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Sesat.</title>
+
+        <link href='https://fonts.googleapis.com/css?family=Josefin+Slab:700' rel='stylesheet' type='text/css'>
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                color: #B0BEC5;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 72px;
+                margin-bottom: 40px;
+            }
+            h1 {font-size: 3em;}
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <h1>404</h1>
+                <div class="title">Sesat? <a href="/"> Kembalilah ke pangkal jalan. </a></div>
+            </div>
+        </div>
+    </body>
+</html>
