120928c8eaaaaaaeaeb21d4cdfe397355008783b
Aidil <aidil@pocketpixel.com>
2016-05-20 16:06:04 +0800

    error 404 v2

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -10,7 +10,8 @@
         <div class="content">
             <div class="row">
                 <div class="col-md-4 col-md-offset-4 text-xs-center m-t-3 m-b-3">
-                    <h1>404</h1>
+                    <img src="images/404.png" alt="404" width="120" class="m-b-3 m-t-3"/>
+                    <h1 style="font-family: "Josefin Slab", serif; font-size: 6rem;">404</h1>
                     <div class="title">Sesat? <a href="/"> Kembalilah ke pangkal jalan. </a></div>
 
                 </div>
