a183329843378b426f103dd406e1a4be36c7955c
faridhub95 <farid_yid13@yahoo.com>
2016-06-17 17:21:12 +0800

    fix authentication

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 
@@ -22,8 +23,28 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+        $user = $request->user();
+
+        if(! $user->type == 'JobSeeker' || ! $user->type == 'employer'){
+
+            if ( $user && $user->type == 'job_seeker'){
+
+                    $user->assignRole('job_seeker');
+
+            }elseif($user && $user->type == 'employer'){
+
+                    $user->assignRole('employer');
+
+            }else{
+
+                 abort(401, 'You are not allowed.');
+
+            }
+        }
+
         return view('home');
     }
+
 }
