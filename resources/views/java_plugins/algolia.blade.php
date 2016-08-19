7d6eb9c14bf87c408cfeff58c7054c54dfde1c2f
faridhub95 <farid_yid13@yahoo.com>
2016-08-19 18:12:22 +0800

    quick fix

--- resources/views/java_plugins/algolia.blade.php
+++ resources/views/java_plugins/algolia.blade.php
@@ -21,7 +21,7 @@ var index = client.initIndex(itsIndex);
 //autocomplete function configurations
 autocomplete('#search-box', { hint: false }, [
     {
-      source: autocomplete.sources.hits(index, { hitsPerPage: 5 }),
+      source: autocomplete.sources.hits(index, { hitsPerPage: 6 }),
       displayKey: 'job_title',
       templates: {
         suggestion: function(suggestion) {
@@ -92,7 +92,7 @@ search.addWidget(
 	  	empty: noResultsTemplate,
 	    item: resultsTemplate
 	  },
-	  hitsPerPage: 4
+	  hitsPerPage: 20
 	})
 );
 
