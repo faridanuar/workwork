b97b7648e4cc1a155ef42e0a0a4a376a457f2cd9
Aidil <aidil@pocketpixel.com>
2016-09-09 10:11:33 +0800

    UI new advert

--- resources/views/adverts/create.blade.php
+++ resources/views/adverts/create.blade.php
@@ -6,23 +6,23 @@
 	@include('messages.flash')
 </div>
 
-<h1>Need a Part-timer?</h1>
+<div class="panel-ww-advert panel panel-default center-block">
+    <div class="panel-heading panel-heading-ww">New Part-time Ad</div>
+    <div class="panel-body">
+	    <form method="post" action="/" enctype="multipart/form-data">
+			@if (count($errors) > 0)
+			<div class="alert alert-danger">
+				<ul>
+					@foreach ($errors->all() as $error)
+					<li>{{ $error }}</li>
+					@endforeach
+				</ul>
+			</div>
+			@endif
 
-<hr>
-
-<div class="row">
-	<form method="post" action="/" enctype="multipart/form-data" class="col-md-6">
-		@if (count($errors) > 0)
-		<div class="alert alert-danger">
-			<ul>
-				@foreach ($errors->all() as $error)
-				<li>{{ $error }}</li>
-				@endforeach
-			</ul>
-		</div>
-		@endif
-		
-		@include('adverts.form')
-	</form>
+			@include('adverts.form')
+		</form>
+    </div>
 </div>
+
 @stop
