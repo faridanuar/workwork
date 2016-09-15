c53c7104b3d0a0e6f441e3c5e74c44a187380482
Aidil <aidil@pocketpixel.com>
2016-09-15 13:06:50 +0800

    responsive advert fix

--- resources/views/adverts/show.blade.php
+++ resources/views/adverts/show.blade.php
@@ -2,13 +2,13 @@
 @section('body-id', 'advert-view')
 @section('content')
 
-<div class="row">
+<div class="row" id="advert">
     <div class="col-sm-8">
         <div class="flash">
         	@include('messages.flash')
         </div>
 
-        <div id="advert" class="panel panel-default">
+        <div class="panel panel-default">
             <div class="panel-body">
                 <h1 class="job-title">{{ $advert->job_title }}</h1>
                 <div class="salary">
@@ -18,11 +18,11 @@
                 <hr>
                 <div class="business-name"><a href="/company/{{ $advert->employer_id }}/{{ $advert->business_name }}">{{ $advert->business_name }}</a></div>
                 <hr>
-                <div class="category visible-xs-*">
+                <div class="category visible-xs-block">
                     <h3>Category</h3>
                     <span class="label label-default">{{ $advert->category }}</span>
                 </div>
-                <div class="skill visible-xs-*">
+                <div class="skill visible-xs-block">
                     <h3>Skills</h3>
                     <!-- <span class="label label-default">{{ $advert->skill }}</span> -->
                     <div class="skill-description">
@@ -39,7 +39,7 @@
                 </div>
                 -->
 
-                <div class="location visible-xs-*">
+                <div class="location visible-xs-block">
                     <h3>Location</h3>
                     {{ $advert->location }}
                     <hr>
@@ -58,12 +58,12 @@
                     {!! nl2br(e($advert->description)) !!}
                 </div>
 
-                <div class="business-name visible-xs-*">
+                <div class="business-name visible-xs-block">
                     <hr>
                     <a href="/company/{{ $advert->employer_id }}/{{ $advert->business_name }}">{{ $advert->business_name }}</a>
                 </div>
                 <!-- <div class="location">{{ $advert->location }}</div> -->
-                <div class="address visible-xs-*">
+                <div class="address visible-xs-block">
                     <div class="street">{{ $advert->street }}</div>
                     <div class="city">{{ $advert->city }}</div>
                     <div class="zip">{{ $advert->zip }}</div>
@@ -71,7 +71,7 @@
                     <hr>
                 </div>
 
-                <div class="visible-xs-*">
+                <div class="visible-xs-block">
                     @if ($authorize === true)
 
                     	@can('edit_advert')
@@ -126,31 +126,32 @@
                     <div class="zip">{{ $advert->zip }}</div>
                     <div class="state">{{ $advert->state }}</div>
                 </div>
-            </div>
-            @if ($authorize === true)
+                <div class="actions">
+                    @if ($authorize === true)
+                        @can('edit_advert')
+                            <a href="/adverts/{{ $advert->id }}/{{ strtolower($advert->job_title) }}/edit" class="btn btn-primary">edit</a>
+                            @if($advert->published === 0)
+                                <form method="post" action="/adverts/publish">
+                                    {{ csrf_field() }}
+                                    <input type="hidden" id="id" name="id" value="{{ $advert->id }}" />
+                                    <button type="submit" class="btn btn-primary">Publish</button>
+                                </form>
+                            @else
+                                <form method="post" action="/adverts/unpublish">
+                                    {{ csrf_field() }}
+                                    <input type="hidden" id="id" name="id" value="{{ $advert->id }}" />
+                                    <button type="submit" class="btn btn-primary">Unpublish</button>
+                                </form>
+                            @endif
+                        @endcan
 
-                @can('edit_advert')
-                    <a href="/adverts/{{ $advert->id }}/{{ strtolower($advert->job_title) }}/edit" class="btn btn-primary">edit</a>
-                    @if($advert->published === 0)
-                        <form method="post" action="/adverts/publish">
-                            {{ csrf_field() }}
-                            <input type="hidden" id="id" name="id" value="{{ $advert->id }}" />
-                            <button type="submit" class="btn btn-primary">Publish</button>
-                        </form>
-                    @else
-                        <form method="post" action="/adverts/unpublish">
-                            {{ csrf_field() }}
-                            <input type="hidden" id="id" name="id" value="{{ $advert->id }}" />
-                            <button type="submit" class="btn btn-primary">Unpublish</button>
-                        </form>
+                    @elseif ( $asEmployer === false )
+
+                        <a href="/adverts/{{ $advert->id }}/{{ strtolower($advert->job_title) }}/apply" class="btn btn-primary btn-lg btn-block btn-ww-lg">Apply</a>
+
                     @endif
-                @endcan
-
-            @elseif ( $asEmployer === false )
-
-                <a href="/adverts/{{ $advert->id }}/{{ strtolower($advert->job_title) }}/apply" class="btn btn-primary btn-lg btn-block btn-ww-lg">Apply</a>
-
-            @endif
+                </div>
+            </div>
         </div>
     </div>
 </div>
