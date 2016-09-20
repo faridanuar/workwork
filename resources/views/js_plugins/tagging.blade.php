38c2250f06b1f9305c2db6bb57e967192d057c98
faridhub95 <farid_yid13@yahoo.com>
2016-09-20 18:01:29 +0800

    test page for tags added

--- resources/views/js_plugins/tagging.blade.php
+++ resources/views/js_plugins/tagging.blade.php
@@ -4,18 +4,17 @@
 
 <form action="/values" method="post">
 {!! csrf_field() !!}
-<select  name="tags" multiple data-role="tagsinput">
-  <option value="IT">IT</option>
-  <option value="Food">Food</option>
+<select multiple data-role="tagsinput" id="tags" name="tags[]">
+  <option value="Amsterdam">Amsterdam</option>
+  <option value="Washington">Washington</option>
+  <option value="Sydney">Sydney</option>
+  <option value="Beijing">Beijing</option>
+  <option value="Cairo">Cairo</option>
+</select>
 <input type="submit" />
 </form>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tagsinput/0.8.0/bootstrap-tagsinput.min.js"></script>
-<script>
-$('tags').tagsinput({
-    itemValue: 'id'
-});
-</script>
 
 @stop
