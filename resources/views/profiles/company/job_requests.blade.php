5b0dbc3c524a8097de8fc550f4cbd6e6536e23e0
Aidil <aidil@pocketpixel.com>
2016-09-20 17:23:44 +0800

    Company dashboard,adverts,applications UI

--- resources/views/profiles/company/job_requests.blade.php
+++ resources/views/profiles/company/job_requests.blade.php
@@ -2,59 +2,66 @@
 
 @section('content')
 
+
+<a href="/adverts">Back to adverts</a>
+<h2>Job Requests for ...</h2>
+<hr>
 @include('profiles.company.requests_category')
+<div class="row">
+	<div class="col-md-12">
+		<h4>Pending List</h4>
+		@forelse($requestInfos as $requestInfo)
 
-<h1>Job Seekers</h1>
+			<a href="/advert/{{ $requestInfo->advert_id }}/job/requests/{{ $requestInfo->jobSeeker->id }}">
+			<div><h4>Status: {{ $requestInfo->status }}</h4></div>
+			<div><h4>Name: {{ $requestInfo->jobSeeker->user->name }}</h4></div>
+			<div><h4>Age: {{ $requestInfo->jobSeeker->age }}</h4></div>
+			<div><h4>Introduction: {{ $requestInfo->introduction }}</h4></div>
+			</a>
+			@if($requestInfo->responded === 0 )
+				<form action="/advert/job/requests/{{ $requestInfo->id }}/response" method="post">
+				{{ csrf_field() }}
 
-	<h4>Pending List</h4>
-	@foreach($requestInfos as $requestInfo)
-	
-		<a href="/advert/{{ $requestInfo->advert_id }}/job/requests/{{ $requestInfo->jobSeeker->id }}">
-		<div><h4>Status: {{ $requestInfo->status }}</h4></div>
-		<div><h4>Name: {{ $requestInfo->jobSeeker->user->name }}</h4></div>
-		<div><h4>Age: {{ $requestInfo->jobSeeker->age }}</h4></div>
-		<div><h4>Introduction: {{ $requestInfo->introduction }}</h4></div>
-		</a>
-		@if($requestInfo->responded === 0 )
-			<form action="/advert/job/requests/{{ $requestInfo->id }}/response" method="post">
-			{{ csrf_field() }}
+					<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#reasonModal" >Respond</button>
 
-				<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#reasonModal" >Respond</button>
+					<!-- Modal 1-->
+					<div id="reasonModal" class="modal fade" role="dialog">
+					  <div class="modal-dialog">
 
-				<!-- Modal 1-->
-				<div id="reasonModal" class="modal fade" role="dialog">
-				  <div class="modal-dialog">
+					    <!-- reason Modal content-->
+					    <div class="modal-content">
+					      <div class="modal-header">
+					        <button type="button" class="close" data-dismiss="modal">&times;</button>
+					        <h4 class="modal-title">Your Response</h4>
+					      </div>
+					      <div class="modal-body">
+						        <div class="btn-group" data-toggle="buttons">
+								  <label class="btn btn-primary">
+								    <input type="radio" name="status" id="status0" value="REJECTED" autocomplete="off" required> Reject
+								  </label>
+								  <label class="btn btn-primary">
+								    <input type="radio" name="status" id="status1" value="ACCEPTED FOR INTERVIEW" autocomplete="off"> Accept for Interview
+								  </label>
+								</div>
+								<div>
+								<h4>Send a Message</h4>
+							        <textarea type="text" name="comment" id="comment" class="form-control" rows="10" placeholder="Setup an interview OR give feedback for rejecting" required>{{ old('comment') }}</textarea>
+									<button type="submit" name="response" id="response0" class="btn btn-primary">Submit</button>
+								</div>
+						   </div>
+					      <div class="modal-footer">
+					        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					      </div>
+					    </div>
+					  </div>
+					</div>
+				</form>
+			@endif
+		@empty
+		<p>Looks like there's no job applications yet.</p>
+		@endforelse
+		{!! $requestInfos->render() !!}
+	</div>
+</div>
 
-				    <!-- reason Modal content-->
-				    <div class="modal-content">
-				      <div class="modal-header">
-				        <button type="button" class="close" data-dismiss="modal">&times;</button>
-				        <h4 class="modal-title">Your Response</h4>
-				      </div>
-				      <div class="modal-body">
-					        <div class="btn-group" data-toggle="buttons">
-							  <label class="btn btn-primary">
-							    <input type="radio" name="status" id="status0" value="REJECTED" autocomplete="off" required> Reject
-							  </label>
-							  <label class="btn btn-primary">
-							    <input type="radio" name="status" id="status1" value="ACCEPTED FOR INTERVIEW" autocomplete="off"> Accept for Interview
-							  </label>
-							</div>
-							<div>
-							<h4>Send a Message</h4>
-						        <textarea type="text" name="comment" id="comment" class="form-control" rows="10" placeholder="Setup an interview OR give feedback for rejecting" required>{{ old('comment') }}</textarea>
-								<button type="submit" name="response" id="response0" class="btn btn-primary">Submit</button>
-							</div>
-					   </div>
-				      <div class="modal-footer">
-				        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-				      </div>
-				    </div>
-				  </div>
-				</div>
-			</form>
-		@endif
-
-	@endforeach
-	{!! $requestInfos->render() !!}
 @stop
