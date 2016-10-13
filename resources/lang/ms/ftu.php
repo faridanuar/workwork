364ddd3e70315e0536da3fe5b05887d0bea7b3d6
Aidil <aidil@pocketpixel.com>
2016-10-13 08:22:20 +0000

    FTU

--- resources/lang/ms/ftu.php
+++ resources/lang/ms/ftu.php
@@ -1,7 +1,9 @@
 <?php
+// FTU - ms
 
 return [
     'business_profile' => 'Pertama, ceritakan tentang bisnes atau organisasi anda',
     'new_ad' => 'Ok, jom reka iklan part-time pertama anda',
-
+    'choose_plan' => 'Berapa lama boleh orang ramai melihat iklan part-time anda?',
+    'payment' => 'Sikit lagi',
 ];
